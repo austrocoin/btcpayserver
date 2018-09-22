a3bae8dfe7b6af5234fcaf2cf814d2dac599370b
austrocoin <austrocoin@gmail.com>
2018-09-22 10:04:48 +0200

    Update HomeController.cs

--- BTCPayServer/Controllers/HomeController.cs
+++ BTCPayServer/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BTCPayServer.Models;
-using BTCPayServer.Services.Rates.RateFetcher;
 using NBitcoin.DataEncoders;
 
 namespace BTCPayServer.Controllers
